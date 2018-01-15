5212f598c3d65670a0399afe0a7434e91a5556aa
Alex Andonian <alexandonian@gmail.com>
2018-01-15 16:50:55 -0500

    Add single video testing script

--- test_model.py
+++ test_model.py
@@ -2,20 +2,21 @@
 # load the trained model then forward pass on a given image
 # By Bolei Zhou
 
-import torch
-from torch.autograd import Variable as V
-import torchvision.models as models
-from torchvision import transforms as trn
-from torch.nn import functional as F
 import os
-import numpy as np
-from scipy.misc import imresize as imresize
 import cv2
+import numpy as np
 from PIL import Image
+from scipy.misc import imresize as imresize
+
+import torch
+import torchvision.models as models
+from torch.nn import functional as F
+from torch.autograd import Variable as V
+from torchvision import transforms as trn
+
 
 def load_model(modelID, categories):
     if modelID == 1:
-        model_name = 'resnet50_imagenetpretrained_moments'
         weight_file = 'moments_RGB_resnet50_imagenetpretrained.pth.tar'
         if not os.access(weight_file, os.W_OK):
             weight_url = 'http://moments.csail.mit.edu/moments_models/' + weight_file
@@ -26,51 +27,60 @@ def load_model(modelID, categories):
         if useGPU == 1:
             checkpoint = torch.load(weight_file)
         else:
-            checkpoint = torch.load(weight_file, map_location=lambda storage, loc: storage) # allow cpu
+            checkpoint = torch.load(weight_file, map_location=lambda storage,
+                                    loc: storage)  # allow cpu
 
-        state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
+        state_dict = {str.replace(k, 'module.', ''): v for k, v in checkpoint['state_dict'].items()}
         model.load_state_dict(state_dict)
 
     model.eval()
     return model
 
-def returnTF():
-# load the image transformer
+
+def load_transform():
+    """Load the image transformer."""
     tf = trn.Compose([
-        trn.Resize((224,224)),
+        trn.Resize((224, 224)),
         trn.ToTensor(),
         trn.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
     return tf
 
-dataset = 'moments'
-modelID = 1
 
-# load category
-with open('category_momentsv1.txt') as f:
-    categories = [line.rstrip() for line in f.readlines()]
+def load_categories():
+    """Load categories."""
+    with open('category_momentsv1.txt') as f:
+        return [line.rstrip() for line in f.readlines()]
 
-# load the labels
-model = load_model(modelID, categories)
 
-# load the transformer
-tf = returnTF() # image transformer
+if __name__ == '__main__':
+    modelID = 1
+    dataset = 'moments'
 
-# load the test image
-if os.path.exists('test.jpg'):
-    os.remove('test.jpg')
-img_url = 'http://places2.csail.mit.edu/imgs/demo/IMG_5970.JPG'
-os.system('wget %s -q -O test.jpg' % img_url)
-img = Image.open('test.jpg')
-input_img = V(tf(img).unsqueeze(0), volatile=True)
+    # load categories
+    categories = load_categories()
 
-# forward pass
-logit = model.forward(input_img)
-h_x = F.softmax(logit, 1).data.squeeze()
-probs, idx = h_x.sort(0, True)
+    # load the model
+    model = load_model(modelID, categories)
 
-print(img_url)
-# output the prediction of action category
-print('--Top Actions:')
-for i in range(0, 5):
-    print('{:.3f} -> {}'.format(probs[i], categories[idx[i]]))
+    # load the transformer
+    tf = load_transform()  # image transformer
+
+    # load the test image
+    if os.path.exists('test.jpg'):
+        os.remove('test.jpg')
+    img_url = 'http://places2.csail.mit.edu/imgs/demo/IMG_5970.JPG'
+    os.system('wget %s -q -O test.jpg' % img_url)
+    img = Image.open('test.jpg')
+    input_img = V(tf(img).unsqueeze(0), volatile=True)
+
+    # forward pass
+    logit = model.forward(input_img)
+    h_x = F.softmax(logit, 1).data.squeeze()
+    probs, idx = h_x.sort(0, True)
+
+    print(img_url)
+    # output the prediction of action category
+    print('--Top Actions:')
+    for i in range(0, 5):
+        print('{:.3f} -> {}'.format(probs[i], categories[idx[i]]))
---
+++ test_video.py
@@ -0,0 +1,129 @@
+"""Test pre-trained model on a single video.
+
+Bolei Zhou and Alex Andonian
+
+"""
+
+import os
+import re
+import cv2
+import argparse
+import functools
+import subprocess
+import numpy as np
+from PIL import Image
+import moviepy.editor as mpy
+
+import torch.optim
+import torch.nn.parallel
+from torch.nn import functional as F
+from torch.autograd import Variable
+
+from test_model import load_model, load_categories, load_transform
+
+
+def extract_frames(video_file, num_frames=8):
+    try:
+        os.makedirs(os.path.join(os.getcwd(), 'frames'))
+    except OSError:
+        pass
+
+    output = subprocess.Popen(['ffmpeg', '-i', video_file],
+                              stderr=subprocess.PIPE).communicate()
+    # Search and parse 'Duration: 00:05:24.13,' from ffmpeg stderr.
+    re_duration = re.compile('Duration: (.*?)\.')
+    duration = re_duration.search(str(output[1])).groups()[0]
+
+    seconds = functools.reduce(lambda x, y: x * 60 + y,
+                               map(int, duration.split(':')))
+    rate = num_frames / float(seconds)
+
+    output = subprocess.Popen(['ffmpeg', '-i', video_file,
+                               '-vf', 'fps={}'.format(rate),
+                               '-vframes', str(num_frames),
+                               '-loglevel', 'panic',
+                               'frames/%d.jpg']).communicate()
+    frame_paths = sorted([os.path.join('frames', frame)
+                          for frame in os.listdir('frames')])
+
+    frames = load_frames(frame_paths)
+    subprocess.call(['rm', '-rf', 'frames'])
+    return frames
+
+
+def load_frames(frame_paths, num_frames=8):
+    frames = [Image.open(frame).convert('RGB') for frame in frame_paths]
+    if len(frames) >= num_frames:
+        return frames[::int(np.ceil(len(frames) / float(num_frames)))]
+    else:
+        raise ValueError('Video must have at least {} frames'.format(num_frames))
+
+
+def render_frames(frames, prediction):
+    rendered_frames = []
+    for frame in frames:
+        img = np.array(frame)
+        height, width, _ = img.shape
+        cv2.putText(img, prediction,
+                    (1, int(height / 8)),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1, (255, 255, 255), 2)
+        rendered_frames.append(img)
+    return rendered_frames
+
+
+# options
+parser = argparse.ArgumentParser(description="test TRN on a single video")
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('--video_file', type=str, default=None)
+group.add_argument('--frame_folder', type=str, default=None)
+parser.add_argument('--rendered_output', type=str, default=None)
+parser.add_argument('--num_segments', type=int, default=8)
+args = parser.parse_args()
+
+# Get dataset categories
+categories = load_categories()
+
+# Load RGB model
+model_id = 1
+model = load_model(model_id, categories)
+
+# Load the video frame transform
+transform = load_transform()
+
+# Obtain video frames
+if args.frame_folder is not None:
+    print('Loading frames in {}'.format(args.frame_folder))
+    import glob
+    # here make sure after sorting the frame paths have the correct temporal order
+    frame_paths = sorted(glob.glob(os.path.join(args.frame_folder, '*.jpg')))
+    frames = load_frames(frame_paths)
+else:
+    print('Extracting frames using ffmpeg...')
+    frames = extract_frames(args.video_file, args.num_segments)
+
+# Prepre input tensor
+data = torch.stack([transform(frame) for frame in frames])
+input_var = Variable(data.view(-1, 3, data.size(2), data.size(3)),
+                     volatile=True)
+
+# Make video prediction
+logits = model(input_var).mean(dim=0)
+# logits = model(input_var)
+print(logits.shape)
+# h_x = torch.mean(F.softmax(logits, 1), dim=0).data
+h_x = F.softmax(logits).data
+probs, idx = h_x.sort(0, True)
+
+# Output the prediction.
+video_name = args.frame_folder if args.frame_folder is not None else args.video_file
+print('RESULT ON ' + video_name)
+for i in range(0, 5):
+    print('{:.3f} -> {}'.format(probs[i], categories[idx[i]]))
+
+# Render output frames with prediction text.
+if args.rendered_output is not None:
+    prediction = categories[idx[0]]
+    rendered_frames = render_frames(frames, prediction)
+    clip = mpy.ImageSequenceClip(rendered_frames, fps=4)
+    clip.write_videofile(args.rendered_output)
